fbb545701397bd6d969ee2635c2ca0d81f7fcf40
rcfeliz <ric.feliz@gmail.com>
2025-04-22 21:24:58 -0300

    mensagem

---
+++ R/funcoes_basicas.R
@@ -0,0 +1,7 @@
+plotar <- function(titulo = NULL) {
+  print(glue::glue("meu gráfico se chama: {titulo}"))
+}
+
+triplicar <- function(numero = NULL) {
+  numero*3
+}
---
+++ data-raw/use_funcoes_basicas.R
@@ -0,0 +1,7 @@
+# usando a função de plotar
+
+plotar(titulo = "amoebas")
+
+# usando a função de triplicar
+
+triplicar(8271)
